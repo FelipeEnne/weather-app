a087f5f0d9ba52f51a48d142172cb1ec7b94801a
FelipeEnne <felipeenne@gmail.com>
2020-03-10 19:26:40 -0300

    create the page skeleton

--- dist/main.js
+++ dist/main.js
@@ -86,14 +86,39 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./src/DOMmodel.js":
+/*!*************************!*\
+  !*** ./src/DOMmodel.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _returnCity__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./returnCity */ \"./src/returnCity.js\");\n\n\n\nasync function getAPIData() {\n  const API = '0571f1a1044888615170693425198c8d';\n  const data = await fetch(\n    `https://api.openweathermap.org/data/2.5/weather?q=${Object(_returnCity__WEBPACK_IMPORTED_MODULE_0__[\"default\"])()}&appid=${API}`,\n    { mode: 'cors' },\n  );\n\n  const model = document.getElementById('output-data');\n\n  if (data.ok === false) {\n    model.style.display = 'none';\n    // eslint-disable-next-line no-alert\n    alert(data.statusText);\n  } else {\n    model.style.display = 'block';\n  }\n\n  const result = await data.json();\n\n  return result;\n}\n\nasync function diplayData() {\n  const data = await getAPIData();\n\n  const name = document.getElementById('output-name');\n  const weather = document.getElementById('output-weather');\n  const img = document.getElementById('output-img');\n  const temp = document.getElementById('output-temp');\n  const feel = document.getElementById('output-feel');\n  const minmax = document.getElementById('output-minmax');\n  const humidity = document.getElementById('output-humidity');\n  const wind = document.getElementById('output-wind');\n\n\n  name.innerHTML = `${data.name} , ${data.sys.country}`;\n  weather.innerHTML = `${data.weather[0].main} , ${data.weather[0].description}`;\n  img.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);\n  temp.innerHTML = `${parseFloat(data.main.temp - 273).toFixed(1)} °C`;\n  feel.innerHTML = `Feels like ${parseFloat(data.main.feels_like - 273).toFixed(1)} °C`;\n  minmax.innerHTML = ` Min ${parseFloat(data.main.temp_min - 273).toFixed(1)} °C\n      - Max ${parseFloat(data.main.temp_max - 273).toFixed(1)} °C`;\n  humidity.innerHTML = `Humidity ${data.main.humidity} %`;\n  wind.innerHTML = `Wind ${data.wind.speed} meter/sec`;\n}\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (diplayData);\n\n\n//# sourceURL=webpack:///./src/DOMmodel.js?");
+
+/***/ }),
+
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-eval("console.log(\"felipe\")\n\n//# sourceURL=webpack:///./src/index.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _DOMmodel__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./DOMmodel */ \"./src/DOMmodel.js\");\n\n\nwindow.onload = Object(_DOMmodel__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\n\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/returnCity.js":
+/*!***************************!*\
+  !*** ./src/returnCity.js ***!
+  \***************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nfunction storeCity(city) {\n  const cit = JSON.stringify(city);\n  sessionStorage.setItem(0, cit);\n}\n\ndocument.getElementById('button-getCity').addEventListener('click', () => {\n  const val = document.getElementById('input-getCity').value;\n  storeCity(val);\n});\n\nfunction getCity() {\n  const cit = sessionStorage.getItem(0);\n  return JSON.parse(cit);\n}\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (getCity);\n\n\n//# sourceURL=webpack:///./src/returnCity.js?");
 
 /***/ })
 
